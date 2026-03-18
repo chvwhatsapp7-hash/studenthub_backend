12b37eca19ae00293868434744b12f48660c9e03
satvika-mantri <satvikamantri2005@gmail.com>
2026-03-18 18:36:05 +0530

    registe update2

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
 
     const query = `
       INSERT INTO "User"
-      (full_name,email,password_hash,phone,university,degree,graduation_year,resume_url,linkedin_url,github_url,age,user_type,role_id,created_at,updated_at)
+      (full_name,email,password_hash,phone,university,degree,graduation_year,resume_url,linkedin_url,github_url,age,role_id,created_at,updated_at)
       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,NOW(),NOW())
       RETURNING *
     `;
@@ -52,8 +52,9 @@ export default async function handler(req, res) {
       resume_url,
       linkedin_url,
       github_url,
+      age
       //user_type,  // 👈 ADD THIS
-      1           // student role
+           // student role
     ];
 
     const result = await pool.query(query, values);
