884eeb470cc4b639afea7a16c8f55a9f5c18e30f
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-04-13 22:59:37 +0530

    jobs updated

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -1,6 +1,6 @@
 import pool from "../../lib/db";
 import { cors } from "../../lib/cors";
-import { sendNotificationToAll } from "../../lib/sendNotificationToAll";
+//import { sendNotificationToAll } from "../../lib/sendNotificationToAll";
 
 export default async function handler(req, res) {
  if (cors(req, res)) return;
