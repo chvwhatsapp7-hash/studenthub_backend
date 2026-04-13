aea5b9e8f371321ce5bc0f6a43d08fe752ee6960
chvwhatsapp7-hash <chvwhatsapp7@gmail.com>
2026-04-13 21:35:01 +0530

    Update companies.js

--- pages/api/companies.js
+++ pages/api/companies.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
   if (cors(req, res)) return;
 
   // ✅ authenticate once for ALL methods
-  const user = authenticate(req, res);
-  if (!user) return res.status(401).json({ success: false, message: "Unauthorized" });
+  // const user = authenticate(req, res);
+  // if (!user) return res.status(401).json({ success: false, message: "Unauthorized" });
 
   try {
 
